d575259dbb1a63c87c2fb31a24ed73e9e88855ad
Dev9tkov <idev9tkov@gmail.com>
2019-10-15 16:16:48 +0500

    4.5 Отладка программы

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -2,7 +2,11 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double squarea(int p, int k) {
-        return (0.5 * p / (k + 1)) * (k * 0.5 * p / (k + 1));
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
+        //return (0.5 * p / (k + 1)) * (k * 0.5 * p / (k + 1));
     }
     public static void main(String[] args) {
         double result1 = squarea(6,2 );
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+    @Test
+    public void whenMaxFirst() {
+        int result = SqMax.max(4, 2, 3, 1);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenMaxSecond() {
+        int result = SqMax.max(4, 6, 3, 1);
+        assertThat(result, is(6));
+    }
+    @Test
+    public void whenMaxThird() {
+        int result = SqMax.max(4, 6, 9, 1);
+        assertThat(result, is(9));
+    }
+    @Test
+    public void whenMaxForth() {
+        int result = SqMax.max(3, 1, 2, 8);
+        assertThat(result, is(8));
+    }
+}
