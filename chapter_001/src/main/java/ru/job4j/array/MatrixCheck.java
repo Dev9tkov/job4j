4ad32d9a44a561df63992fc5eb1edbebbfd32d8b
Dev9tkov <idev9tkov@gmail.com>
2019-10-22 12:00:55 +0500

    6.7 Выигрышные комбинации в сокобан

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,40 +3,25 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        boolean start = false;
         int count1 = 0;
         int count2 = 0;
-        int index = 0;
         for (int row = 0; row < board.length; row++) {
-            for (int cell = 0; cell < board.length; cell++) {
-                char sign = board[row][cell];
-                System.out.print(sign);
-                if (sign == 'X') {
-                    if (cell == 0) {
-                        start = true;
+            if (board[row][row] == 'X') {
+                for (int cell = 0; cell < board.length; cell++) {
+                    if (board[row][cell] == 'X') {
                         count1++;
                     }
-                    if (start && cell > 0) {
-                        count1++;
-                    }
-                    if (row == 0) {
-                        index = cell;
+                    if (board[cell][row] == 'X') {
                         count2++;
                     }
-                    if (cell == index && row > 0) {
-                        count2++;
-                    }
-
                 }
-                if (sign == '_') {
-                    start = false;
+                if (count1 == board.length || count2 == board.length) {
+                    result = true;
+                    break;
                 }
+                break;
             }
-
             System.out.println();
-        }
-        if (count1 == board.length || count2 == board.length) {
-            return true;
         }
         return result;
     }
