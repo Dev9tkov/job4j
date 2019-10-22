7f49ebe8b530677ec6942d34f7e2c898dd473c57
Dev9tkov <idev9tkov@gmail.com>
2019-10-22 11:44:13 +0500

    SqArea

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -3,10 +3,9 @@ package ru.job4j.condition;
 public class SqArea {
     public static double squarea(int p, int k) {
         int h = p / (2 * (k + 1));
-        int w = k * h;
-        int s = w * h;
+        int l = k * h;
+        int s = l * h;
         return s;
-        //return (0.5 * p / (k + 1)) * (k * 0.5 * p / (k + 1));
     }
     public static void main(String[] args) {
         double result1 = squarea(6, 2);
