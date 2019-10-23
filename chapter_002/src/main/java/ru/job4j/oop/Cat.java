dd91fbdd4f488bb3258552e24427163d69668115
Dev9tkov <idev9tkov@gmail.com>
2019-10-23 11:31:57 +0500

    1.5 Поля объекта

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -1,13 +1,53 @@
 package ru.job4j.oop;
 
+/**
+ * 1.5 Поля объекта[#187471]
+ * @author Dev9tkov
+ * @version 1
+ * @since 23.10.2019
+ */
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+
+    private String food;
+    private String name;
+
+    /**
+     * Метод вызывает имя котика и что он ел
+     */
+    public void show() {
+        System.out.println(this.name + " el " + this.food);
     }
+
+    /**
+     * Метод записывает в поле объекта значение переменной
+     * @param meat что ел котик
+     */
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    /**
+     * Метод записывает кличку кота в переменную
+     * @param nick кличка котика
+     */
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    /**
+     * Метод создает 2 экземпляра класса и вызывает методы gav, show и giveNick
+     * @param args args
+     */
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
