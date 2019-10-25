4a17971cfa866ada0fc4756a0cb52072f6280318
Dev9tkov <idev9tkov@gmail.com>
2019-10-25 17:18:58 +0500

    1 Экзамен базовый синтаксис

---
+++ chapter_001/src/test/java/ru/job4j/array/Merge.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Merge {
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int m = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                rsl[m] = left[i];
+                i++;
+            } else { rsl[m] = right[j];
+                j++;
+            }
+            m++;
+        }
+        while (i < left.length) {
+            rsl[m] = left[i];
+            i++;
+            m++;
+        }
+        while (j < right.length) {
+            rsl[m] = right[j];
+            j++;
+            m++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
