2fa6b5a72eb10489c668285816a917712765c187
Dev9tkov <idev9tkov@gmail.com>
2019-10-26 11:42:24 +0500

    2.2 Рефакторинг - площадь треугольника

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import org.junit.Assert;
+import ru.job4j.calculator.Fit;
 
 public class TriangleTest {
     @Test
@@ -9,10 +9,12 @@ public class TriangleTest {
         Point n = new Point(2, 6);
         Point o = new Point(0, 3);
         Point k = new Point(1, 4);
-
         Triangle tr = new Triangle(n, o, k);
-        double rsl = tr.area();
-        assertThat(rsl, is(0.5));
+        double expected = tr.area();
+        double out = 0.5;
+        Assert.assertEquals(expected, out, 0.01);
     }
 }
+
+
 
