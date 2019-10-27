f9b74157fabb8202a0d545b692436e55c6252130
Dev9tkov <idev9tkov@gmail.com>
2019-10-27 12:54:26 +0500

    2 Перегрузка конструктора

--- chapter_002/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,12 +1,14 @@
 package ru.job4j.tracker;
 
 public class Animal {
+
     public Animal() {
-        super();
-        System.out.println("Animal");
+    }
+
+    public Animal(String name) {
     }
 
     public static void main(String[] args) {
-        Tiger tiger = new Tiger();
+        Animal animal = new Animal();
     }
 }
