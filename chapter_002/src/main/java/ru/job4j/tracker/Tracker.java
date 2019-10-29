13a1df6abfbe83956a1d5d4f901c505430afed5d
Dev9tkov <idev9tkov@gmail.com>
2019-10-29 15:52:57 +0500

    5 Реализовать класс Tracker

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,87 @@
 package ru.job4j.tracker;
 
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+    /**
+     * Указаткль ячейки для новой заявки
+     */
+    private int position = 0;
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        for (int i = 0; i < items.length; i++) {
+            if (this.items[i].getId().equals(id)) {
+                this.items[i] = item;
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        for (int i = 0; i < items.length; i++) {
+            if (this.items[i].getId().equals(id)) {
+                this.items[i] = null;
+                System.arraycopy(items, 0, items, i + 1,items.length - 1);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[this.position];
+        for (int i = 0; i != this.position; i++) {
+            result[i] = this.items[i];
+            }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        int k = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && this.items[i].getName().equals(key)) {
+                k++;
+            }
+        }
+        Item[] result = new Item[k];
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && this.items[i].getName().equals(key)) {
+                result[i] = this.items[i];
+            }
+        }
+        return result;
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,88 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.describedAs;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1");
+        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+        tracker.add(previous);
+        // Создаем новую заявку.
+        Item next = new Item("test2");
+        // Проставляем старый id из previous, который был сгенерирован выше.
+        next.setId(previous.getId());
+        // Обновляем заявку в трекере.
+        tracker.replace(previous.getId(), next);
+        // Проверяем, что заявка с таким id имеет новые имя test2.
+        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
+
+    @Test
+    public void whenDeleteIdThenReturnTrue() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        boolean result = tracker.delete(first.getId());
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenFindAllThenReturnItems() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        Item[] result = tracker.findAll();
+        Item[] expect = {first, second, third};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindByNameThenReturnName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        Item[] result = tracker.findByName("test1");
+        Item[] expect = {first};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindByIdThenReturnName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        assertThat(tracker.findById(third.getId()).getName(), is("test3"));
+    }
+
+}
