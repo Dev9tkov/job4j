e679656e88cbca0a6da2e3b76f079fb0ecce686a
Dev9tkov <idev9tkov@gmail.com>
2019-10-31 06:53:48 +0500

    3 Интерфейс Input

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport {
+
+
+    @Override
+    public void ehat() {
+    }
+
+    @Override
+    public void pass(int chislo) {
+    }
+
+    @Override
+    public int zapravka(int toplivo) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public interface Transport {
+    void ehat();
+
+    void pass(int chislo);
+
+    int zapravka(int toplivo);
+}
