4dd680de2d7211c149b7089e243cbc7ae0d1806b
Dev9tkov <idev9tkov@gmail.com>
2019-11-15 09:37:01 +0500

    3. Изменить программу Tracker из 2-го модуля[#187420]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -13,27 +13,13 @@ public class StartUI {
             run = action.execute(input, tracker);
         }
     }
-//    public void init(Input input, Tracker tracker, UserAction[] actions) {
-//        boolean run = true;
-//        while (run) {
-//            this.showMenu(actions);
-//            int select = input.askInt("Select: ", actions.length);
-//            UserAction action = actions[select];
-//            run = action.execute(input, tracker);
-//        }
-//    }
+
         private void showMenu(ArrayList<UserAction> actions) {
             System.out.println("Menu.");
             for(UserAction value : actions) {
                 System.out.println(value.info());
             }
         }
-//    private void showMenu(UserAction[] actions) {
-//        System.out.println("Menu.");
-//        for (int index = 0; index < actions.length; index++) {
-//            System.out.println(actions[index].info());
-//        }
-//    }
 
     public static void main(String[] args) {
         Input input = new ConsoleInput();
@@ -47,15 +33,6 @@ public class StartUI {
         actions.add(new FindbyIdAction(4, "Find Item by ID"));
         actions.add(new FindByNameAction(5, "Find Items by name"));
         actions.add(new FindByNameAction(6, "Exit"));
-//        UserAction[] actions = {
-//                new CreateAction(0, "Create Item"),
-//                new FindAllAction(1, "Show all Items"),
-//                new ReplaceAction(2, "Replace Item"),
-//                new DeleteAction(3, "Delete Item"),
-//                new FindbyIdAction(4, "Find Item by ID"),
-//                new FindByNameAction(5, "Find Items by name"),
-//                new ExitAction(6, "Exit")
-//        };
         new StartUI().init(validate, tracker, actions);
     }
 }
