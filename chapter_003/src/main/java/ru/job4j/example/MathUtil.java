e2248dab9f2f26127ef0cb9bae391b8952eee5dc
Dev9tkov <idev9tkov@gmail.com>
2019-11-20 11:45:31 +0500

    0.1 Ссылки на методы.[#187289]

---
+++ chapter_003/src/main/java/ru/job4j/example/MathUtil.java
@@ -0,0 +1,18 @@
+package ru.job4j.example;
+
+/**
+ * To do
+ *
+ * @author Ilya Devyatkov
+ * @version 01
+ * @since 20.11.2019
+ */
+public class MathUtil {
+    public static double add(int left, int second) {
+        return left + second;
+    }
+
+    public static double div(int left, int second) {
+        return left / second;
+    }
+}
