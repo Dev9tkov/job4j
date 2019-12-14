b387e48a074bda0e01ad12ca79cf00c3d4b02b21
Dev9tkov <idev9tkov@gmail.com>
2019-12-14 15:20:01 +0500

    1. Реализовать коллекцию Set на массиве[#208149]

---
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,55 @@
+package ru.job4j.set;
+
+import ru.job4j.list.SimpleArrayList;
+import java.util.Iterator;
+
+/**
+ * Реализовать коллекцию
+ * Коллекция не должна хранить дубликаты
+ *
+ * @author Ilya Devyatkov
+ * @version 01
+ * @since 14.12.2019
+ */
+public class SimpleSet<E> implements Iterable<E> {
+
+    private SimpleArrayList<E> list = new SimpleArrayList<>();
+
+    /**
+     * Добавление нового элемента
+     * @param value
+     */
+    public void add(E value) {
+        if(!dublicate(value)) {
+            list.add(value);
+        }
+    }
+
+    /**
+     * Првоерка на наличие дубликатов
+     * @param value
+     * @return
+     */
+    public boolean dublicate(E value) {
+        boolean result = false;
+        for (E model : list) {
+            if(model.equals(value)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Вспомогательный метод для теста
+     * @return
+     */
+    public int size() {
+        return this.list.getSize();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return this.list.iterator();
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.set;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * To do
+ *
+ * @author Ilya Devyatkov
+ * @version 01
+ * @since 14.12.2019
+ */
+public class SimpleSetTest {
+    @Test
+    public void f() {
+        SimpleSet<Integer> set = new SimpleSet();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        assertThat(set.size(), is(3));
+    }
+}
